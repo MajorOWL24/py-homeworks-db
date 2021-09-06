8ef6c3346eb5cf274323a2ba9484a6d7440d39b8
MajorOWL24 <81160767+MajorOWL24@users.noreply.github.com>
2021-09-06 22:38:39 +1000

    Add foreign keys

--- creation/script.sql
+++ creation/script.sql
@@ -9,15 +9,18 @@ CREATE TABLE IF NOT EXISTS public.albums
 (
     name character varying,
     id integer,
-    "year of issue" smallint
+    "year of issue" smallint,
+    performer_id integer
 );
 
 CREATE TABLE IF NOT EXISTS public.tracks
 (
     name character varying,
     id integer,
-    "length of time" smallint
+    "length of time" smallint,
+    album_id integer
 );
+
 
 ALTER TABLE public."Performers"
     ADD PRIMARY KEY (id);
@@ -27,3 +30,17 @@ ALTER TABLE public.albums
 
 ALTER TABLE public.tracks
     ADD PRIMARY KEY (id);
+
+
+ALTER TABLE public.albums
+    ADD CONSTRAINT performer_album FOREIGN KEY (performer_id)
+    REFERENCES public."Performers" (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+    
+ALTER TABLE public.tracks
+    ADD CONSTRAINT album_track FOREIGN KEY (album_id)
+    REFERENCES public.albums (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
